e96d5040daaa37da0c9fb606658d3eabb895746c
rutv1k46 <rutviksolanki01@gmail.com>
2024-08-05 14:13:19 -0400

    children routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,25 @@ import { Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 export const routes: Routes = [
     {
-        path: '',
+        path: '',   // <domain>
         component: NoTaskComponent
     },
     {
-        path: "users/:userId",
+        path: "users/:userId",  // <domain>/users/<uid>
         component: UserTasksComponent,
+        children: [
+            {
+                path: 'tasks',  // <domain>/users/<uid>/tasks
+                component: TasksComponent
+            },
+            {
+                path: 'tasks/new', // <domain>/users/<uid>/tasks/new
+                component: NewTaskComponent
+            }
+        ]
     },
 ];
