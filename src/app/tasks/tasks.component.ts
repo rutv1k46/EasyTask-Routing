0c7e3702829576ba50699e0b63bb42c17a46ac40
rutv1k46 <rutviksolanki01@gmail.com>
2024-08-05 14:35:48 -0400

    programmatic routing

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,19 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from "@angular/core";
 
-import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
+import { TaskComponent } from "./task/task.component";
+import { TasksService } from "./tasks.service";
 
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+    selector: "app-tasks",
+    standalone: true,
+    templateUrl: "./tasks.component.html",
+    styleUrl: "./tasks.component.css",
+    imports: [TaskComponent],
 })
 export class TasksComponent {
-  userId = input.required<string>()
-  userTasks: Task[] = [];
+    userId = input.required<string>();
+    private tasksService = inject(TasksService);
+    userTasks = computed(() =>
+        this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+    );
 }
