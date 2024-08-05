dc8859c0860f5e9cd807c18604e118374a5f5c91
rutv1k46 <rutviksolanki01@gmail.com>
2024-08-05 14:57:14 -0400

    fallback route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,20 @@
-import { ApplicationConfig } from "@angular/core";
-import { routes } from "./app.routes";
+import { ApplicationConfig } from '@angular/core';
 import {
-    provideRouter,
-    withComponentInputBinding,
-    withRouterConfig,
-} from "@angular/router";
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(
-            routes,
-            withComponentInputBinding(),
-            withRouterConfig({ paramsInheritanceStrategy: "always" })
-        ),
-    ],
+  providers: [
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+      })
+    ),
+  ],
 };
