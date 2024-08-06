1cb899702f78bf0c29375570f2bad211c6cbe017
rutv1k46 <rutviksolanki01@gmail.com>
2024-08-06 15:15:28 -0400

    lazy load routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,42 +1,48 @@
-import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 
-import { routes as userRoutes } from './users/users.routes';
-import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { UserTasksComponent, resolveTitle, resolveUserName } from './users/user-tasks/user-tasks.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { inject } from '@angular/core';
+import { routes as userRoutes } from "./users/users.routes";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import {
+    UserTasksComponent,
+    resolveTitle,
+    resolveUserName,
+} from "./users/user-tasks/user-tasks.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
 
 const dummyCanMatch: CanMatchFn = (route, segment) => {
-    const router = inject(Router)
-    const shouldGetAccess = Math.random()
-    if(shouldGetAccess < 1){
-        return true
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+    if (shouldGetAccess < 1) {
+        return true;
     }
-    return new RedirectCommand(router.parseUrl('/unauthorized'))
-}
+    return new RedirectCommand(router.parseUrl("/unauthorized"));
+};
 
 export const routes: Routes = [
-  {
-    path: '', // <your-domain>/
-    component: NoTaskComponent,
-    // redirectTo: '/users/u1',
-    // pathMatch: 'full'
-  },
-  {
-    path: 'users/:userId', // <your-domain>/users/<uid>
-    component: UserTasksComponent,
-    children: userRoutes,
-    canMatch: [dummyCanMatch],
-    data: {
-      message: 'Supp Bitch!'
+    {
+        path: "", // <your-domain>/
+        component: NoTaskComponent,
+        // redirectTo: '/users/u1',
+        // pathMatch: 'full'
     },
-    resolve: {
-      userName: resolveUserName
+    {
+        path: "users/:userId", // <your-domain>/users/<uid>
+        component: UserTasksComponent,
+        // children: userRoutes,
+        loadChildren: () =>
+            import("./users/users.routes").then((mod) => mod.routes),
+        canMatch: [dummyCanMatch],
+        data: {
+            message: "Supp Bitch!",
+        },
+        resolve: {
+            userName: resolveUserName,
+        },
+        title: resolveTitle,
     },
-    title: resolveTitle
-  },
-  {
-    path: '**',
-    component: NotFoundComponent,
-  },
+    {
+        path: "**",
+        component: NotFoundComponent,
+    },
 ];
--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,34 +1,10 @@
-import { ResolveFn, Routes } from "@angular/router";
-
+import { Routes } from "@angular/router";
 
 import {
     canLeaveEditPage,
     NewTaskComponent,
 } from "../tasks/new-task/new-task.component";
-import { Task } from "../tasks/task/task.model";
-import { TasksService } from "../tasks/tasks.service";
-import { inject } from "@angular/core";
-
-export const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
-};
+import { resolveUserTasks, TasksComponent } from "../tasks/tasks.component";
 
 export const routes: Routes = [
     {
@@ -38,10 +14,11 @@ export const routes: Routes = [
     },
     {
         path: "tasks", // <your-domain>/users/<uid>/tasks
-        loadComponent: () =>
-            import("../tasks/tasks.component").then(
-                (mod) => mod.TasksComponent
-            ),
+        component: TasksComponent,
+        // loadComponent: () =>
+        //     import("../tasks/tasks.component").then(
+        //         (mod) => mod.TasksComponent
+        //     ),
         runGuardsAndResolvers: "always",
         resolve: {
             userTasks: resolveUserTasks,
